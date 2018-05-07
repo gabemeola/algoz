f3d6079d03ff9e1c6f8dc7a9f0fde4a9ea6eb9bd
Gabe M <gabemeola@gmail.com>
2018-05-06 18:11:23 -0600

    buzzzzz

---
+++ src/fizzBuzz/fizzBuzz.js
@@ -0,0 +1,16 @@
+function fizzBuzz(end = 100) {
+  for (let i = 1; i <= end; i++) {
+    let str = '';
+    if (i % 3 === 0) {
+      str += 'Fizz'
+    }
+
+    if (i % 5 === 0) {
+      str += 'Buzz'
+    }
+
+    console.log(str || i)
+  }
+}
+
+fizzBuzz()
